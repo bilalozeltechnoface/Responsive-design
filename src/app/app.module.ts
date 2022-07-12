03d01b4ac2c124f542c54344ee619c7291f14082
bilalozeltechnoface <97604672+bilalozeltechnoface@users.noreply.github.com>
2022-07-12 15:30:50 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { Section1Component } from './section1/section1.component';
+import { CardComponent } from './card/card.component';
+import { ParallaxComponent } from './parallax/parallax.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [							
+    AppComponent,
+      HeaderComponent,
+      BannerComponent,
+      FooterComponent,
+      Section1Component,
+      CardComponent,
+      ParallaxComponent,
+      ContactComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
